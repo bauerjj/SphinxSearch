826993a574a0ef5648429558d17dc7795ddc3c5c
Justin Bauer <kapotchy@gmail.com>
2013-02-27 00:09:29 -0700

    Deleted unused constants

--- sphinxconstants.php
+++ sphinxconstants.php
@@ -1,12 +1,7 @@
 <?php
 
-define ('OUTPUT_FILE', PATH_PLUGINS . DS . 'SphinxSearch' . DS . 'install' . DS . 'output.txt');
-define ('PID_FILE', PATH_PLUGINS . DS . 'SphinxSearch' . DS . 'install' . DS . 'pid.txt');
-define ('ERROR_FILE', PATH_PLUGINS . DS . 'SphinxSearch' . DS . 'install' . DS . 'error.txt');
-define ('STOP_WORDS_FILE', PATH_PLUGINS . DS . 'SphinxSearch' . DS . 'assests'. DS .'stop_words.txt');
-define ('WORD_FORMS_FILE', PATH_PLUGINS . DS . 'SphinxSearch' . DS . 'assests'. DS .'word_forms.txt');
-
-define ('SS_INSTALL_DIR', PATH_PLUGINS . DS . 'SphinxSearch' . DS. 'install'); //install path if using packaged installer
+define ('STOP_WORDS_FILE', PATH_PLUGINS . DS . 'SphinxSearchLite' . DS . 'assests'. DS .'stop_words.txt');
+define ('WORD_FORMS_FILE', PATH_PLUGINS . DS . 'SphinxSearchLite' . DS . 'assests'. DS .'word_forms.txt');
 
 //error logs
 define ('SS_SUCCESS', 1);
@@ -14,10 +9,8 @@ define ('SS_WARNING', 2);
 define ('SS_FATAL_ERROR', 3);
 
 //@todo should be a setting
-define ('SS_PREVIEW_BODY_LIMIT', 150); //tooltip hover preview limit
+define ('SS_PREVIEW_BODY_LIMIT', 150); //@todo delete this - tooltip hover preview limit
 define ('SS_BODY_LIMIT', 1000); //regular body text limit in main search
-
-
 
 define('SS_DELTA_INDEX', 'delta'); //there is still a prefix
 define('SS_MAIN_INDEX', 'main');
@@ -29,9 +22,9 @@ define('SS_STATS_INDEX', 'stats');
  *
  */
 //individual index names - rotate these by themselves
-define ('SS_INDEX_MAIN',C('Plugin.SphinxSearch.Prefix', 'vss_').'main');
-define ('SS_INDEX_DELTA',C('Plugin.SphinxSearch.Prefix', 'vss_').'delta');
-define ('SS_INDEX_STATS',C('Plugin.SphinxSearch.Prefix', 'vss_').'stats');
+define ('SS_INDEX_MAIN',C('Plugin.SphinxSearchLite.Prefix', 'vss_').'main');
+define ('SS_INDEX_DELTA',C('Plugin.SphinxSearchLite.Prefix', 'vss_').'delta');
+define ('SS_INDEX_STATS',C('Plugin.SphinxSearchLite.Prefix', 'vss_').'stats');
 
 //distributed index - search against this, but don't update this on itself
 define ('SS_INDEX_DIST','vanilla');
