66480c7b289fe39d3e820ceacf016ca9cb316a16
Justin Bauer <kapotchy@gmail.com>
2013-12-05 22:32:43 -0700

    Deleted unused settings and updated the plugin's dashboard to reflect this

--- views/sphinxsearch.php
+++ views/sphinxsearch.php
@@ -3,52 +3,6 @@ if (!defined('APPLICATION'))
     exit();
 ?>
 
-<script type="text/javascript" charset="utf-8">
-    function addmsg(type, msg){
-        $("#messages").html(
-        "<div class='msg Inner "+ type +"'>"+ msg +"</div>"
-    );
-    }
-
-    function updatestatus(status){
-        $("#Status").html(
-        "<div class='StatusMsg'>Status: "+ status +"</div>"
-    );
-    }
-
-    function waitForMsg(){
-        var WebRoot = $("#WebRoot").val();
-        $.ajax({
-            dataType: "json",
-            type: "GET",
-            url: WebRoot+"/plugin/sphinxsearch/ServicePoll",
-
-            async: true, /* If set to non-async, browser shows page as "Loading.."*/
-            cache: false,
-            timeout:50000, /* Timeout in ms */
-
-            success: function(data){ /* called when request to barge.php completes */
-                addmsg("new", data.Terminal); /* Add response to a .msg div (with the "new" class)*/
-                updatestatus(data.Status);
-                setTimeout(
-                'waitForMsg()', /* Request next message */
-                1000 /* ..after 1 seconds */
-            );
-            },
-            error: function(XMLHttpRequest, textStatus, errorThrown){
-                addmsg("error", textStatus + " (" + errorThrown + ")");
-                setTimeout(
-                'waitForMsg()', /* Try again after.. */
-                "15000"); /* milliseconds (15seconds) */
-            }
-        });
-    };
-
-    $(document).ready(function(){
-        waitForMsg(); /* Start the inital request */
-    });
-</script>
-
 <style type="text/css" media="screen">
     body{ background:#000;color:#fff;font-size:.9em; }
     .old{ background-color:#246499;}
@@ -145,48 +99,12 @@ if (!defined('APPLICATION'))
 <h3>Requirements</h3>
 <div class="Info">
     <ol>
-        <li>Linux Only!</li>
+        <li>Manual install via distro, yourself, or installer (windows)</li>
         <li>PHP >= 5.3.0</li>
-        <li>Shell Access</li>
-        <li>Spawn a Daemon (searchd)</li>
-        <li>Port Forwarding</li>
     </ol>
 </div>
 <h3>Control Panel</h3>
 <br/>
-<?php echo $this->Form->Label('Run in background: ', 'Background', array('class' => 'background')); ?>
-<?php echo $this->Form->RadioList('Background', array(TRUE => 'True', FALSE => 'False'), array('list' => FALSE, 'default' => 'False')) ?>
-<ul class="Settings">
-    <li class="FootNote">This lets all of the index commands to run in the background. Useful for long operations and identifying warnings</li>
-    <li class="FootNote">If running in background, terminal output is presented below in the black box</li>
-</ul>
-<table class="CPanel Overall">
-    <tbody>
-        <tr>
-            <th class="Desc">General: </th>
-            <th>Indexer</th>
-            <th>Searchd</th>
-            <th>Config</th>
-            <th>Uptime</th>
-            <th>Total Queries</th>
-            <th>Maxed Out</th>
-
-
-        </tr>
-        <tr>
-            <td class="Desc">Status: </td>
-            <td><?php if ($Settings['Status']->IndexerFound) Success('Found'); else Fail('Not Installed'); ?></td>
-            <td><?php if ($Settings['Status']->SearchdFound) Success('Found'); else Fail('Not Installed'); ?></td>
-            <td><?php if ($Settings['Status']->ConfFound) Success('Found'); else Fail('Not Found'); ?></td>
-            <td><?php echo Gdn_Format::Seconds($Settings['Status']->Uptime) ?></td>
-            <td><?php echo Gdn_Format::BigNumber($Settings['Status']->TotalQueries) ?></td>
-            <td><?php echo Gdn_Format::BigNumber($Settings['Status']->MaxedOut) ?></td>
-        </tr>
-
-    </tbody>
-</table>
-<br/>
-
 <div id="ControlPanel">
     <table class="CPanel Index">
         <tbody>
@@ -196,81 +114,31 @@ if (!defined('APPLICATION'))
                 <th>Delta</th>
                 <th>Stats</th>
             </tr>
-            <tr>
-                <td class="Desc">Count of Docs:</td>
-                <td>
-                    <?php echo Gdn_Format::BigNumber($Settings['Status']->IndexerMainTotal) ?>
-                    <?php echo $this->Form->Button('Action', array('class' => 'SmallButton', 'value' => 'Reload Main')); ?>
-                </td>
-                <td>
-                    <?php echo Gdn_Format::BigNumber($Settings['Status']->IndexerDeltaTotal) ?>
-                    <?php echo $this->Form->Button('Action', array('class' => 'SmallButton', 'value' => 'Reload Delta')); ?>
-                </td>
-                <td>
-                    <?php echo Gdn_Format::BigNumber($Settings['Status']->IndexerStatsTotal) ?>
-                    <?php echo $this->Form->Button('Action', array('class' => 'SmallButton', 'value' => 'Reload Stats')); ?>
-                </td>
-            </tr>
-            <tr>
-                <td class="Desc">Last Index Time:</td>
-                <td><?php echo $Settings['Status']->IndexerMainLast == '---' ? '----' : Gdn_Format::FuzzyTime($Settings['Status']->IndexerMainLast) ?></td>
-                <td><?php echo $Settings['Status']->IndexerDeltaLast == '---' ? '----' : Gdn_Format::FuzzyTime($Settings['Status']->IndexerDeltaLast) ?></td>
-                <td><?php echo $Settings['Status']->IndexerStatsLast == '---' ? '----' : Gdn_Format::FuzzyTime($Settings['Status']->IndexerStatsLast) ?></td>
-            </tr>
         <td class="Desc">cron Files: </td>
         <td><?php echo Anchor(T('cron file'), 'plugin/sphinxsearch/viewfile/' . Gdn::Session()->TransientKey() . '?action=viewfile&file=maincron', array('target' => '_blank')) ?>
-            <?php echo $this->Form->Button('Action', array('class' => 'SmallButton', 'value' => 'Write Main Cron')); ?>
 
         </td>
         <td><?php echo Anchor(T('cron file'), 'plugin/sphinxsearch/viewfile/' . Gdn::Session()->TransientKey() . '?action=viewfile&file=deltacron', array('target' => '_blank')) ?>
-            <?php echo $this->Form->Button('Action', array('class' => 'SmallButton', 'value' => 'Write Delta Cron')); ?>
 
         </td>
         <td><?php echo Anchor(T('cron file'), 'plugin/sphinxsearch/viewfile/' . Gdn::Session()->TransientKey() . '?action=viewfile&file=statscron', array('target' => '_blank')) ?>
-            <?php echo $this->Form->Button('Action', array('class' => 'SmallButton', 'value' => 'Write Stats Cron')); ?></td>
-        </tr>
-        <tr>
-            <td class="Desc">Actions:  </td>
-            <td>
-                <?php echo $this->Form->Button('Action', array('class' => 'SmallButton', 'value' => 'Index Main')); ?>
-            </td>
-            <td>
-                <?php echo $this->Form->Button('Action', array('class' => 'SmallButton', 'value' => 'Index Delta')); ?>
-            </td>
-            <td>
-                <?php echo $this->Form->Button('Action', array('class' => 'SmallButton', 'value' => 'Index Stats')); ?>
-            </td>
-        </tr>
 
+        </td>
+        </tr>
         </tbody>
     </table>
-    <ul class="Settings">
-        <li class="FootNote">Indexing will temporarily stop sphinx</li>
-        <li class="FootNote">Indexing `main` may take a long time</li>
-    </ul>
     <br/>
     <table class="CPanel Searchd">
         <tbody>
             <tr>
                 <th class="Desc">Searchd: </th>
                 <th>Status</th>
-                <th>Port</th>
-                <th>Connections</th>
             </tr>
             <tr>
                 <td>Status: </td>
-                <td> <?php if ($Settings['Status']->SearchdRunning) Success('Running'); else Fail('Not Running'); ?> </td>
-                <td><?php if ($Settings['Status']->SearchdPortStatus) Success($Settings['Install']->Port); else Fail($Settings['Install']->Port); ?></td>
-                <td><?php echo Gdn_Format::BigNumber($Settings['Status']->SearchdConnections) ?></td>
+                <td> <?php if ($Settings['Status']->SearchdRunning) Success('Running');
+    else Fail('Not Running'); ?> </td>
             </tr>
-            <tr>
-                <td class="Desc">Actions: </td>
-                <td> <?php echo $this->Form->Button('Action', array('class' => 'SmallButton', 'value' => 'Start Searchd')); ?>
-                    <?php echo $this->Form->Button('Action', array('class' => 'SmallButton', 'value' => 'Stop Searchd')); ?>
-                <td> <?php echo $this->Form->Button('Action', array('class' => 'SmallButton', 'value' => 'Check Port')); ?></td>
-                <td>----</td>
-            </tr>
-
         </tbody>
     </table>
     <br/>
@@ -279,75 +147,48 @@ if (!defined('APPLICATION'))
             <tr>
                 <th class="Desc">Config: </th>
                 <th>Configuration </th>
-            </tr>
-            <tr>
-                <td>Actions:</td>
-                <td> <?php echo Anchor('config file', 'plugin/sphinxsearch/viewfile/' . Gdn::Session()->TransientKey() . '?action=viewfile&file=conf', array('target' => '_blank')); ?>
-                    <?php echo $this->Form->Button('Action', array('class' => 'SmallButton', 'value' => 'Write Config')); ?>
-                </td>
-            </tr>
-        </tbody>
-    </table>
-    <br/>
-    <br/>
-    <table class="CPanel Searchd">
-        <tbody>
-            <tr>
-                <th class="Desc">Debug: </th>
-                <th>Kill </th>
-                <th>Manual Override</th>
                 <th>Search Log</th>
                 <th>Query Log</th>
                 <th>Cron Log</th>
-
             </tr>
             <tr>
-                <td>Actions:</td>
-                <td>
-                    <?php echo $this->Form->Button('Action', array('class' => 'SmallButton', 'value' => 'Kill Searchd(s)')); ?>
+                <td>Status:</td>
+                <td> <?php echo Anchor('config file', 'plugin/sphinxsearch/viewfile/' . Gdn::Session()->TransientKey() . '?action=viewfile&file=conf', array('target' => '_blank')); ?>
                 </td>
                 <td>
-                    <?php if ($Settings['Status']->EnableSphinxSearch) Success('Sphinx Enabled'); else Fail('Sphinx Disabled'); ?>
-                    <?php echo $this->Form->Button('Action', array('class' => 'SmallButton', 'value' => 'Toggle Sphinx Search')); ?>
+<?php echo Anchor(T('search log'), 'plugin/sphinxsearch/viewfile/' . Gdn::Session()->TransientKey() . '?action=viewfile&file=searchlog', array('target' => '_blank')) ?>
                 </td>
                 <td>
-                    <?php echo Anchor(T('search log'), 'plugin/sphinxsearch/viewfile/' . Gdn::Session()->TransientKey() . '?action=viewfile&file=searchlog', array('target' => '_blank')) ?>
+<?php echo Anchor(T('query log'), 'plugin/sphinxsearch/viewfile/' . Gdn::Session()->TransientKey() . '?action=viewfile&file=querylog', array('target' => '_blank')) ?>
                 </td>
-                 <td>
-                    <?php echo Anchor(T('query log'), 'plugin/sphinxsearch/viewfile/' . Gdn::Session()->TransientKey() . '?action=viewfile&file=querylog', array('target' => '_blank')) ?>
-                </td>
-                 <td>
-                    <?php echo Anchor(T('cron log'), 'plugin/sphinxsearch/viewfile/' . Gdn::Session()->TransientKey() . '?action=viewfile&file=cronlog', array('target' => '_blank')) ?>
+                <td>
+<?php echo Anchor(T('cron log'), 'plugin/sphinxsearch/viewfile/' . Gdn::Session()->TransientKey() . '?action=viewfile&file=cronlog', array('target' => '_blank')) ?>
                 </td>
             </tr>
         </tbody>
     </table>
-    <ul class="Settings">
-<li class="FootNote">Kill Searchd is useful for when multiple instances of searchd are running - common sideffect of this is error: WARNING: no process found by PID xxxx </li>
-<li class="FootNote">Manual override will either enable sphinx over the default (if searchd is running), or disable sphinx and let the default run (regardless of the state of searchd)</li>
-</ul>
+    <br/>
+    <br/>
 
-</div>
-<br/>
-<div id="Status">Status: Idle
-</div>
-<div id="messages">
-    <div class="msg">
-        Command Line Output
-        <br/>
-        =====Ready=====
-    </div>
 </div>
 <br/>
 
 <h3>Changelog</h3>
-
+20131124
+<ol>
+    <li> Removed the complicated installer. Now all installs must be done before running the plugin</li>
+    <li> Removed all non-plugin related configurations from the settings menu. User must edit the generated sphinx.conf file directly</li>
+    <li>  Fixed a bug where the number of related threads on the bottom of each discussion was using the limit as inserted in the
+        settings page for the sidebar widget. Now the settings work as intended and operate independent of each LIMIT.</li>
+    <li>fixed "sleek" to "sleak"</li>
+</ol>
+<br/>
 20130330
 <ol>
     <li> Whenever the advanced search is expanded, the div will stay collapsed for subsequent searches until it is toggled</li>
     <li> Fixed a bug where the xx amount of search results were not being reconstructed back to their original ranking order from sphinx. This caused the results within each page to be mixed randomly!</li>
     <li> Fixed a bug where sometimes the results will say "xx results found" but no results actually shown. This is because the default page landing was NOT being set correctly to 1. This is repeatable when a
-            previous search on a large page number is then followed by a search that returns a few results on a smaller pagination scale than the previously viewed one. No results will be shown since the GET query string tells sphinx to return the previous search's offset</li>
+        previous search on a large page number is then followed by a search that returns a few results on a smaller pagination scale than the previously viewed one. No results will be shown since the GET query string tells sphinx to return the previous search's offset</li>
     <li>Instead of checking if sphinx is installed and ready, the plugin now forces the default search to ALWAYS be sphinx until the plugin is disabled. Any errors should now be spit out on any page that fetches a query from sphinx</li>
     <li>Added a message indicating that apache may not have the correct read/write permissions</li>
 </ol>
@@ -420,8 +261,6 @@ if (!defined('APPLICATION'))
     <li>Initial Release</li>
 </ol>
 <br/>
-
-<?php echo $this->Form->Close() ?>
 
 <?php
 
